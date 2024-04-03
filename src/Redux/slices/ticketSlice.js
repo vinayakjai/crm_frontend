126cb058bb96b4209bcc7781508512f44c0c75b6
vinayakjain <vinaaykjain388@gmail.com>
2024-04-03 12:03:21 +0530

    added ticketslice to maintain ticket state

--- src/Redux/slices/ticketSlice.js
+++ src/Redux/slices/ticketSlice.js
@@ -1,16 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axiosInstance from "../../helpers/axiosinstance";
 
 const initialState={
    ticketList:[],
 
 }
 
+export const getAllTickets=createAsyncThunk("tickets/getAllTicketsForUsers",async()=>{
+    try{
+        const response=await axiosInstance.get("getMyAssignedTickets",{
+            'x-access-token':localStorage.getItem('token'),
+        })
+        return response;
+    }catch(err){
+        return err
+    }
+})
+
 const ticketSlice=createSlice({
     name:'ticket',
     initialState,
     reducer:{},
     extraReducers:(builder)=>{
-
+        
+      builder.addCase(getAllTickets.fulfilled,(state,action)=>{
+        if(!action?.payload?.data) return;
+        state.ticketList=action?.payload?.data?.result;
+      })
     }
 
 })
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,31 @@
+import { useEffect } from "react";
 import Card from "../../components/Card";
 import HomeLayout from "../../layouts/homeLayout";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllTickets } from "../../Redux/slices/ticketSlice";
 
 function Home() {
+
+  //hooks declaration-------------
+  const dispatch=useDispatch();
+
+  //accessing state from store
+  const authState=useSelector((state)=>state.auth);
+  const ticketState=useSelector((state)=>state.ticketState);
+
+  //defining functions here-------
+  async function loadAllTickets(){
+   const response=await dispatch(getAllTickets());
+   if(response.payload.data){
+     
+   }
+  }
+ 
+
+  //useEffect ----------
+  useEffect(()=>{
+     loadAllTickets();
+  },[authState.token]);
   return (
     <HomeLayout>
       <Card />
